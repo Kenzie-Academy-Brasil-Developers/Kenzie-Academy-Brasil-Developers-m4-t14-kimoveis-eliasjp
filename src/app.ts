e130494b7fabd5140a3d3a75950f9fc723757101
eliasjp <illumine1998@gmail.com>
2023-03-03 01:49:38 -0300

    create login logic

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,13 @@ import { handleError } from "./errors"
 import { usersRoutes } from "./routes/users.route"
 import "express-async-errors"
 import "reflect-metadata"
+import { loginRoute } from "./routes/login.route"
 
 const app: Application = express()
 app.use(express.json())
 
 app.use("/users", usersRoutes)
+app.use("/login", loginRoute)
 
 app.use(handleError)
 
--- src/errors.ts
+++ src/errors.ts
@@ -10,13 +10,17 @@ class AppError extends Error {
 }
 
 function handleError (error: any, request: Request, response: Response, _: NextFunction){
-    console.log(`Esperando Error`)
+    console.log(`handleError called`)
     if (error instanceof AppError){
+        console.log(`handleError (AppError) called`)
         return response.status(error.statusCode).json({ message: error.message })
     }
     if (error instanceof ZodError){
+        console.log(`handleError (ZodError) called`)
         return response.status(400).json({ message: error.flatten().fieldErrors })
     }
+    console.log(`handleError (anything else) called`)
+    return response.status(500).json({ message: "Server internal error" })
 }
 
 export { AppError, handleError }
