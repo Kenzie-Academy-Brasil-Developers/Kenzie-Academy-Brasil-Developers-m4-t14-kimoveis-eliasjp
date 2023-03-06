8d688d6675ad9f825c5dc508594c8ac4fd7eb5b2
eliasjp <illumine1998@gmail.com>
2023-03-06 09:37:16 -0300

    create function read RealEstate

--- src/routes/realEstate.route.ts
+++ src/routes/realEstate.route.ts
@@ -1,5 +1,6 @@
 import { Router } from "express"
 import { createRealEstateController } from "../controllers/realEstate.controller"
+import { readRealEstateController } from "../controllers/realEstate.controller"
 import { checkAdmin } from "../middlewares/checkAdmin.middleware"
 import { checkToken } from "../middlewares/checkToken.middleware"
 import { verifySchema } from "../middlewares/verifySchema.middleware"
@@ -8,3 +9,4 @@ import { realEstateSchema } from "../schemas/realEstate.schema"
 export const realEstateRoutes: Router = Router()
 
 realEstateRoutes.post("", checkToken, checkAdmin, verifySchema(realEstateSchema), createRealEstateController)
+realEstateRoutes.get("", readRealEstateController)
---
+++ src/services/realEstate/readRealEstate.services.ts
@@ -0,0 +1,11 @@
+import { AppDataSource } from "../../data-source"
+import { RealEstate } from "../../entities"
+
+export async function readRealEstateService (request: any){
+    const realEstateRepo = AppDataSource.getRepository(RealEstate)
+    const realEstateList = await realEstateRepo.createQueryBuilder("re")
+    .leftJoinAndSelect("re.address", "address")
+    .getMany()
+
+    return realEstateList
+}
